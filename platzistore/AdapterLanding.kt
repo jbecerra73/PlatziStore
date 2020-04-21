49a46abd145786d1535d134b464b4a6514765289
ximo73 <ximobecerra@gmail.com>
2020-04-21 14:54:06 -0300

    Update AdapterLanding.kt

--- platzistore/AdapterLanding.kt
+++ platzistore/AdapterLanding.kt
@@ -1,11 +1,18 @@
 package com.example.platzistore
 
+import android.app.Activity
+import android.content.Intent
+import androidx.core.util.Pair
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityOptionsCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.item_landing.view.*
+import org.jetbrains.anko.startActivity
+import java.net.URI.create
 
-class AdapterLanding ( val data: List<ItemLanding?>?): RecyclerView.Adapter<AdapterLanding.Holder>(){
+class AdapterLanding(val data: List<ItemLanding?>?) : RecyclerView.Adapter<AdapterLanding.Holder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder =
         Holder(parent?.inflate(R.layout.item_landing))
@@ -15,17 +22,28 @@ class AdapterLanding ( val data: List<ItemLanding?>?): RecyclerView.Adapter<Adap
             holder?.bindView(it[position])
         }
     }
+    override fun getItemCount(): Int = data?.size ?: 0
 
-    override fun getItemCount(): Int = data?.size   ?:0
-
-
-    class Holder(itemView: View):RecyclerView.ViewHolder(itemView){             //itemView fue declarado en la clase
-        fun bindView(itemLanding: ItemLanding?){
+    class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
+        fun bindView(itemLanding: ItemLanding?) {
             itemLanding?.let {
-                with(it){
+                with(it) {
                     itemView.txtTitleItem.text = title
                     itemView.txtDescItem.text = desc
-                    itemView.txtPriceItem.text = "$ ${String.format("%.2f", price)}"        //%.2f = 2 numeros despues de la coma
+                    itemView.txtPriceItem.text = "$ ${String.format("%.2f", price)}"
+
+                    itemView.setOnClickListener {
+                        val intent = Intent(itemView.context, DetailActivity::class.java)
+                        intent.putExtra("title", title)
+                        intent.putExtra("desc", desc)
+                        intent.putExtra("price", price)
+
+                        val p1: Pair<View, String> = Pair.create(itemView.imgItemHeader, "transitionHeader")
+                        val p2: Pair<View, String> = Pair.create(itemView.txtTitleItem, "transitionTitle")
+                        val p3: Pair<View, String> = Pair.create(itemView.txtDescItem, "transitionDesc")
+                        val p4: Pair<View, String> = Pair.create(itemView.txtPriceItem, "transitionPrice")
+                        val options: ActivityOptionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(itemView.context as Activity, p1, p2, p3, p4)
+                        itemView.context.startActivity(intent, options.toBundle())  }
                 }
             }
         }
